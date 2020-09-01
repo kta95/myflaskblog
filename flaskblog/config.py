8de8f378dcd55b6ff017f9dd4e97de45ff984eb7
Kyaw Thet Aung <billionairekta@gmail.com>
2020-09-01 15:55:00 +0630

    split with blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '3bba0ac5ddaf96c34168af61af0e0d97'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
